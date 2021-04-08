70ea50cf067fc966d123c821be42dbee09893717
alkan <alkanminecraft@hotmail.com>
2021-04-08 16:21:53 +0200

    a

--- socket_client.py
+++ socket_client.py
@@ -5,5 +5,8 @@ import sys
 
 ip = socket.gethostname()
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.connect((ip, 1234))
+server.connect(("192.168.0.148", 1234))
 
+while True:
+    data = server.recv(8)
+    print(data)
--- socket_server.py
+++ socket_server.py
@@ -3,12 +3,13 @@ import os
 import sys
 
 
-
+ip = "0.0.0.0"
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(("0.0.0.0", 1234))
+server.bind((ip, 1234))
 server.listen(5)
 
 while True:
     connection , address = server.accept()
     print("connected with", address)
+    connection.send(bytes("it works"))
 

